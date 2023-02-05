5db359c0a68716a1ad1bab55e7413698ad864f2e
Ben Arnold <bfarnold@gmail.com>
2023-02-04 17:50:56 -0800

    Add configuration file

---
+++ R/0-wuha-srr-Config.R
@@ -0,0 +1,25 @@
+#----------------------------
+# 0-wuha-srr-Config.R
+#
+# Configuration file for the
+# WUHA study longitudinal 
+# analysis of seroconversion
+# and seroreversion (SRR)
+# 
+# based on Ct Pgp3 IgG
+#----------------------------
+
+#----------------------------
+# load required packages
+#----------------------------
+library(here)
+library(tidyverse)
+library(ggplot2)
+library(lme4)
+library(cowplot)
+
+#-------------------------------
+# local file paths
+#-------------------------------
+box_data_path <- "~/Library/CloudStorage/Box-Box/trachoma-endgame/data"
+
